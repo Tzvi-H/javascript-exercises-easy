afa192a3963ac2204314e61302e8d3ef39f93b37
Tzvi-H <16guitar@gmail.com>
2019-12-19 16:05:59 +0800

    completed exercise 3

---
+++ list_processing/multiply_all_pairs.js
@@ -0,0 +1,16 @@
+// Write a function that takes two array arguments, each containing a list of numbers, and returns a new array containing the products of all combinations of number pairs that exist between the two arrays. The returned array should be sorted in ascending numerical order.
+
+// You may assume that neither argument will be an empty array.
+
+// Example:
+
+// multiplyAllPairs([2, 4], [4, 3, 1, 2]);    // [2, 4, 4, 6, 8, 8, 12, 16]
+
+function multiplyAllPairs(arr1, arr2) {
+  return arr1.reduce((acc, num1) => {
+    arr2.forEach(num2 => acc.push(num1 * num2));
+    return acc;
+  }, []).sort((a, b) => a - b);
+}
+
+console.log(multiplyAllPairs([2, 4], [4, 3, 1, 2]));    // [2, 4, 4, 6, 8, 8, 12, 16]
