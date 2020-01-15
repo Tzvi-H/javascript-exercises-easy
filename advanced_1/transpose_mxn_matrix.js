81f8fca586529a37fd526189bfe56fde596d1618
Tzvi-H <16guitar@gmail.com>
2020-01-15 12:43:16 +0800

    completed transpose_mxn_matrix

---
+++ advanced_1/transpose_mxn_matrix.js
@@ -0,0 +1,31 @@
+function transpose(matrix) {
+  let transposedMatrix = [];
+
+  for (let index = 0; index < matrix[0].length; index += 1) {
+    let temp = [];
+    for (let inner = 0; inner < matrix.length; inner += 1) {
+      temp.push(matrix[inner][index]);
+    }
+    transposedMatrix.push(temp);
+  }
+
+  return transposedMatrix;
+}
+
+const matrix = [
+  [1, 5, 8, 5],
+  [4, 7, 2, 0],
+  [3, 9, 6, 1]
+];
+
+let newMatrix = transpose(matrix);
+
+console.log(newMatrix);      // [[1, 4, 3], [5, 7, 9], [8, 2, 6], [5, 0, 1]]
+console.log(matrix);         // [[1, 5, 8, 5], [4, 7, 2, 0], [3, 9, 6, 1]]
+
+console.log(transpose([[1, 2, 3, 4]]));            // [[1], [2], [3], [4]]
+console.log(transpose([[1], [2], [3], [4]]));      // [[1, 2, 3, 4]]
+console.log(transpose([[1]]));                     // [[1]]
+
+console.log(transpose([[1, 2, 3, 4, 5], [4, 3, 2, 1, 0], [3, 7, 8, 6, 2]]));
+// [[1, 4, 3], [2, 3, 7], [3, 2, 8], [4, 1, 6], [5, 0, 2]]
